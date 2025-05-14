75a4fe8e612a13bf09b1176254d4f6c8719e7301
skylerkuo <kuo10529@gmail.com>
2025-05-14 15:56:15 +0800

    Update combine_qwen.py

--- combine_qwen.py
+++ combine_qwen.py
@@ -1,28 +1,26 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from peft import PeftModel
 import torch
-import os
 
 base_model_path = "Qwen/Qwen2.5-0.5B-Instruct"
-lora_checkpoint_path = "outputs/checkpoint-300"
+lora_checkpoint_path = "outputs/checkpoint-6000"
 push_dir = "qwen_finetune_v1"
 
 tokenizer = AutoTokenizer.from_pretrained(base_model_path, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
 
-base_model = AutoModelForCausalLM.from_pretrained(
+base_model = AutoModelForSequenceClassification.from_pretrained(
     base_model_path,
-    torch_dtype=torch.float16,
+    num_labels=2,
     device_map="auto",
+    torch_dtype=torch.float16,
     trust_remote_code=True
 )
 
-# === Apply LoRA adapter and merge ===
 print("Loading LoRA and merging weights...")
 model = PeftModel.from_pretrained(base_model, lora_checkpoint_path)
 model = model.merge_and_unload()
 
-# === Save merged model and tokenizer ===
 print(f"Saving merged model to: {push_dir}")
 model.save_pretrained(push_dir)
 tokenizer.save_pretrained(push_dir)
